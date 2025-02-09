99f1418107439493a7dd1f64561ce664f97608a1
Fabio Mogavero <fm@fabiomogavero.com>
2025-02-09 20:26:25 +0100

    New test script

---
+++ test.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+################################################################################
+#                                                                              #
+# HypatiaFSA Webpage                                                           #
+#                                                                              #
+# test.sh                                                                      #
+#                                                                              #
+# Copyright (C) 2025, the Hypatia Development Team                             #
+# All rights reserved.                                                         #
+#                                                                              #
+################################################################################
+
+## Begin of file test.sh ##
+
+bundle exec jekyll serve
+
+## End of file test.sh ##
